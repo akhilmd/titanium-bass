d35081c0ba3eea1a32b28e4ac403222944a7896b
DarkFate13 <ganesh3597@gmail.com>
2017-03-13 09:51:05 +0530

    Added function declarations

--- include/backend/backend.h
+++ include/backend/backend.h
@@ -1,6 +1,24 @@
+#include <stdio.h>
+#include <stdio.h>
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <sys/stat.h>
 
 using namespace std;
 
-string select ();
+bool create_DB(string DB_name);
+bool create_table(string table_name, string table_columns[]);
+bool insert_into_table(string table_name, string row_values[]);
+bool check_DB_exists(string DB_name);
+bool check_table_exists(string table_name);
+string *sort_by_param(string table_name, string param)	//for groupby
+string *retrive_info(string table_name, string columns[]) // For select statments
+
+/** 
+
+ More functions will be added
+ If more functions are needed, please add them here
+
+**/
+
